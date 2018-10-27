46bffa1a5861adb0cb6eadc472ad90aa3b1a3e86
Ádám Kovács <35578457+multiversetheorie@users.noreply.github.com>
2018-10-27 22:30:51 +0200

    Parsed earthquake time as long and displayed it as formatted date/time.

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -47,7 +47,7 @@ public final class QueryUtils {
         // Catch the exception so the app doesn't crash, and print the error message to the logs.
         try {
 
-            // TODO: Parse the response given by the SAMPLE_JSON_RESPONSE string and
+            // Parse the response given by the SAMPLE_JSON_RESPONSE string and
             // build up a list of Earthquake objects with the corresponding data.
 
             // Convert SAMPLE_JSON_RESPONSE String into a JSONObject
